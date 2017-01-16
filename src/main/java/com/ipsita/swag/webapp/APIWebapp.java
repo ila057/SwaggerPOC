e263df1bd7d1fbbe091382090ab707e18a461155
Ipsita Samal <ipsita@bluejeansnet.com>
2017-01-16 17:58:05 +0530

    Working POC of swagger with enunciate

--- src/main/java/com/ipsita/swag/webapp/APIWebapp.java
+++ src/main/java/com/ipsita/swag/webapp/APIWebapp.java
@@ -2,10 +2,6 @@ package com.ipsita.swag.webapp;
 
 import com.google.gson.Gson;
 import com.ipsita.swag.service.APIService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -14,20 +10,14 @@ import java.util.List;
 import java.util.Map;
 
 @Path("resources")
-
-@Api( value = "/userAPIs", description = "return user Info")
 public class APIWebapp {
 	private static APIService helperService = new APIService();
 
-	@ApiOperation(
-			value = "get api",
-			notes = "get api notes"
-	)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "success"),
-			@ApiResponse(code = 500, message = "error")
-	})
-
+	/**
+	 * sth
+	 * @param id sth
+	 * @return sth
+	 */
 	@Path("getRequest/{id}/")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -39,16 +29,11 @@ public class APIWebapp {
 
 	}
 
-
-	@ApiOperation(
-			value = "put api",
-			notes = "put api notes"
-	)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "success"),
-			@ApiResponse(code = 500, message = "error")
-	})
-
+	/**
+	 * sth
+	 * @param id sth
+	 * @return sth
+	 */
 	@Path("putRequest/{id}/")
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
@@ -60,15 +45,13 @@ public class APIWebapp {
 
 	}
 
-	@ApiOperation(
-			value = "post api",
-			notes = "post api notes"
-	)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "success"),
-			@ApiResponse(code = 500, message = "error")
-	})
 
+	/**
+	 * sth
+	 * @param id sth
+	 * @param value sth
+	 * @return sth
+	 */
 	@Path("postRequest/{id}/")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
@@ -78,15 +61,12 @@ public class APIWebapp {
 
 	}
 
-	@ApiOperation(
-			value = "delete api",
-			notes = "delete api notes"
-	)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "success"),
-			@ApiResponse(code = 500, message = "error")
-	})
 
+	/**
+	 * sth
+	 * @param id sth
+	 * @return sth
+	 */
 	@Path("deleteRequest/{id}/")
 	@DELETE
 	@Produces(MediaType.APPLICATION_JSON)
