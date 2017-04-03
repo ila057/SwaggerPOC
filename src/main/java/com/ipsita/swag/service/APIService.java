6d6bd6be11d52a122d3abf985268d1be59b78ae5
Ipsita Samal <ipsita@bluejeansnet.com>
2017-04-03 14:30:53 +0530

    Swagger using enunciate gradle plugin

--- src/main/java/com/ipsita/swag/service/APIService.java
+++ src/main/java/com/ipsita/swag/service/APIService.java
@@ -17,16 +17,16 @@ public class APIService {
 
 		Map<String, String > result1 = new HashMap();
 		result1.put("id", String.valueOf(id));
-		result1.put("AccountCategory", "ENTERPRISE");
-		result1.put("BJNUserTypeField", "ENTERPRISE_FREETRIAL");
-		result1.put("BJNProduct", "MEET_ME");
+		result1.put("AccountCategory", "FREE");
+		result1.put("UserTypeField", "SPECIAL");
+		result1.put("Product", "STYLUS");
 		result1.put("BillingStatus","INACTIVE");
 
 		Map<String, String > result2 = new HashMap();
 		result2.put("id", String.valueOf(id));
-		result2.put("AccountCategory", "INDIVIDUAL");
-		result2.put("BJNUserTypeField", "INDIVIDUAL_EXPIRED");
-		result2.put("BJNProduct", "PRIMETIME");
+		result2.put("AccountCategory", "PAIN");
+		result2.put("UserTypeField", "PREMIUM");
+		result2.put("Product", "RANDOMUS");
 		result2.put("BillingStatus","ACTIVE");
 
 		resultList.add(result1);
